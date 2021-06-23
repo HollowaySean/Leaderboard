dca9c0f739298094904b4eb33de262ab803c7622
HollowaySean <hollowayseanm@gmail.com>
2021-06-23 11:10:31 -0700

    Tasks and debug

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:8080');
+    console.log('Example app listening at http://localhost:' + port);
 });

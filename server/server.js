71b3de859a574d70ec788bba4f0c266b704d2521
HollowaySean <hollowayseanm@gmail.com>
2021-06-24 18:50:50 -0700

    Database query methods

--- server/server.js
+++ server/server.js
@@ -6,4 +6,9 @@ const app = express();
 
 const ratingDB = require('./src/ratingDB');
 
-ratingDB.usersInGroup(0, (results) => console.log(results));
+ratingDB.usersInGroup(1, (results) => console.log(results));
+ratingDB.decksInGroup(0, (results) => console.log(results));
+ratingDB.decksInUser(0, (results) => console.log(results));
+ratingDB.groupsWithUser('sean', (results) => console.log(results));
+ratingDB.userWithID(1, (results) => console.log(results));
+ratingDB.groupWithID(1, (results) => console.log(results));
--- server/src/ratingDB.js
+++ server/src/ratingDB.js
@@ -29,10 +29,97 @@ function usersInGroup(groupID, callback) {
     );
 }
 
+// Get all decks in group
+function decksInGroup(groupID, callback) {
+
+    // MySQL query
+    db.query('SELECT deck FROM `group_members` WHERE groupID = ' + mysql.escape(groupID) + ';', 
+        function (err, results) {
+            if(err) throw err;
+
+            // Pack array of data
+            let decks = [];
+            for(let i = 0; i < results.length; i++){
+                decks.push(results[i].deck);
+            }
+
+            // Send callback after completion
+            callback(decks);
+        }
+    );
+}
 
 // Get all decks in group
+function decksInUser(userID, callback) {
+
+    // MySQL query
+    db.query('SELECT deck FROM `group_members` WHERE userID = ' + mysql.escape(userID) + ';', 
+        function (err, results) {
+            if(err) throw err;
+
+            // Pack array of data
+            let decks = [];
+            for(let i = 0; i < results.length; i++){
+                decks.push(results[i].deck);
+            }
+
+            // Send callback after completion
+            callback(decks);
+        }
+    );
+}
 
 // Get all groups that contain a user
+function groupsWithUser(userID, callback) {
 
+    // MySQL query
+    db.query('SELECT DISTINCT groupID FROM `group_members` WHERE userID = ' + mysql.escape(userID) + ';', 
+        function (err, results) {
+            if(err) throw err;
+
+            // Pack array of data
+            let users = [];
+            for(let i = 0; i < results.length; i++){
+                users.push(results[i].groupID);
+            }
+
+            // Send callback after completion
+            callback(users);
+        }
+    );
+}
+
+// Convert userID to user name
+function userWithID(userID, callback) {
+
+    // MySQL query
+    db.query('SELECT userName FROM `group_members` WHERE userID = ' + mysql.escape(userID) + ' LIMIT 1;', 
+        function (err, results) {
+            if(err) throw err;
+
+            // Send callback after completion
+            callback(results[0].userName);
+        }
+    );
+}
+
+// Convert groupID to group name
+function groupWithID(groupID, callback) {
+
+    // MySQL query
+    db.query('SELECT groupName FROM `group_members` WHERE groupID = ' + mysql.escape(groupID) + ' LIMIT 1;', 
+        function (err, results) {
+            if(err) throw err;
+
+            // Send callback after completion
+            callback(results[0].groupName);
+        }
+    );
+}
 
 exports.usersInGroup = usersInGroup;
+exports.decksInGroup = decksInGroup;
+exports.groupsWithUser = groupsWithUser;
+exports.userWithID = userWithID;
+exports.groupWithID = groupWithID;
+exports.decksInUser = decksInUser;

31bffbbd547a63d716beec99caf4da8adaf98645
HollowaySean <hollowayseanm@gmail.com>
2021-06-30 17:47:14 -0700

    Update History.js

--- client/src/App/History.js
+++ client/src/App/History.js
@@ -21,11 +21,11 @@ export default function History() {
                         data={[{ name: 1, uv : 1, pv: 2}, { name: 2, uv: 2, pv: 1}]}
                         margin={{top: 5, right: 20, left: 10, bottom: 5}}
                     >
-                        <XAxis dataKey="name" />
-                        <YAxis />
                         <CartesianGrid stroke='#f5f5f5' />
                         <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0}/>
                         <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1}/>
+                        <XAxis dataKey="name" />
+                        {/* <YAxis yAxisId="test"/> */}
                     </LineChart>
                 </div>
             </div>
